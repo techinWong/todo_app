bd539f0118b17de7b4370b9af73ecdb9380e38ea
techin <techin.won@innosoft.kmutt.ac.th>
2021-11-04 16:47:30 +0700

    change function component to class component
    change function component to class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import ControlElement from './ControlElement';
 
-function App() {
-  return (
+class App extends React.Component {
+  render(){
+    return (
       <div className="mainContainer">
         <div className="titles">
           <h1 className="title">To-Do App</h1>
         </div>
         <ControlElement />
       </div>
-      
-  );
+    );
+    }
 }
 
 export default App;
